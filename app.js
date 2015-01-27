54d0e932f9cf17d2f7964509daf3ed8a93b5192d
Joseph McCarron <joe@zendesk.com>
2015-01-27 15:08:04 -0800

    cleanup and finish

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 (function() {
-
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.created':'grab'
     },
-
-    doSomething: function() {
+    requests: {
+      'updateTicket': function(assignee) {
+        return {
+          url: '/api/v2/tickets/' + this.ticket().id(),
+          type: 'PUT',
+          dataType: 'JSON',
+          contentType: 'application/JSON',
+          data: JSON.stringify({"ticket":
+            {
+              "assignee_id": assignee
+            }
+          })
+        };
+      }
+    },
+    grab: function() {
+      if(this.ticket().postSaveAction() == 'next_play_ticket') {
+        console.log("Play enabled");
+        var me = this.currentUser().id(),
+            ticket = this.ticket().id();
+        if(this.setting('force')) {
+          // assign with AJAX
+          this.ajax('updateTicket', me)
+          .done(function(response) {
+            console.log(response);
+          });
+        } else {
+          // set assignee on current ticket
+          this.ticket().assignee({'userId': me});
+        }
+      }
     }
   };
 
